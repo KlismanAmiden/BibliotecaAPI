76494c7a5d081f32fa639b09fb0ca804e6561439
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-10 07:51:40 -0300

    feat: add find and exists methods for ISBN

--- src/main/java/com/backend/Biblioteca/infrastructure/repository/LivroRepository.java
+++ src/main/java/com/backend/Biblioteca/infrastructure/repository/LivroRepository.java
@@ -4,9 +4,11 @@ import com.backend.Biblioteca.domain.model.Livro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface LivroRepository extends JpaRepository<Livro, Long> {
     List<Livro> findByAutoresId(Long autorId);
     List<Livro> findByGenerosId(Long generoId);
-
+    Optional<Livro> findByIsbn(String isbn);
+    boolean existsByIsbn(String isbn);
 }
