b3a5d7d358e45ab75c8901800a1244093bfbb81c
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-12 00:02:09 -0300

    test: add list author success test

--- src/test/java/com/backend/Biblioteca/application/service/AutorServiceTest.java
+++ src/test/java/com/backend/Biblioteca/application/service/AutorServiceTest.java
@@ -1,11 +1,19 @@
 package com.backend.Biblioteca.application.service;
 
+import com.backend.Biblioteca.application.dto.response.AutorResponseDTO;
 import com.backend.Biblioteca.domain.model.Autor;
 import com.backend.Biblioteca.infrastructure.repository.AutorRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class AutorServiceTest {
@@ -25,4 +33,20 @@ public class AutorServiceTest {
         autor.setNacionalidade(nacionalidade);
         return autor;
     }
+    @Test
+    void deveListarAutoresComSucesso() {
+        Autor autor1 = criarAutor(1L, "Isaac Asimov", "Escritor de ficção científica", 1920, "Americana");
+        Autor autor2 = criarAutor(2L, "Machado de Assis", "Escritor brasileiro", 1839, "Brasileira");
+
+        when(repository.findAll()).thenReturn(List.of(autor1, autor2));
+
+        List<AutorResponseDTO> resultado = service.listarTodos();
+
+        assertEquals(2, resultado.size());
+        assertEquals("Isaac Asimov", resultado.get(0).nome());
+        assertEquals("Machado de Assis", resultado.get(1).nome());
+
+        verify(repository).findAll();
+    }
+
 }
