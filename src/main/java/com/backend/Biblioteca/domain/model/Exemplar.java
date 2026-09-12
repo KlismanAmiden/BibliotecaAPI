0e869f74cf1f269939747da0add4239f36db583d
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-12 01:15:11 -0300

    feat: add book copy model

---
+++ src/main/java/com/backend/Biblioteca/domain/model/Exemplar.java
@@ -0,0 +1,31 @@
+package com.backend.Biblioteca.domain.model;
+
+import com.backend.Biblioteca.domain.enums.StatusExemplar;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Exemplares")
+@Getter
+@Setter
+public class Exemplar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "livro_id",nullable = false)
+    private Livro livro;
+
+    @Column(nullable = false)
+    private String codigo;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "status",nullable = false)
+    private StatusExemplar status = StatusExemplar.DISPONIVEL;
+
+
+
+}
