7ca6dbabbf90e2d22339383eb06bbf97b60a14fc
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-16 16:11:22 -0300

    feat: add authentication controller

---
+++ src/main/java/com/backend/Biblioteca/web/controller/AuthController.java
@@ -0,0 +1,23 @@
+package com.backend.Biblioteca.web.controller;
+
+import com.backend.Biblioteca.application.dto.request.LoginRequestDTO;
+import com.backend.Biblioteca.application.dto.response.LoginResponseDTO;
+import com.backend.Biblioteca.application.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService service;
+
+    @PostMapping("/login")
+    public LoginResponseDTO login(@RequestBody LoginRequestDTO dto) {
+        return service.login(dto);
+    }
+}
