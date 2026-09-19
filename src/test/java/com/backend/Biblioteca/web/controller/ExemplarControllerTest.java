0aa9d64153dde721f4104c10b9d70a450a57f5fc
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-19 19:10:19 -0300

    test: add exemplar controller tests

---
+++ src/test/java/com/backend/Biblioteca/web/controller/ExemplarControllerTest.java
@@ -0,0 +1,39 @@
+package com.backend.Biblioteca.web.controller;
+
+import com.backend.Biblioteca.application.dto.request.ExemplarRequestDTO;
+import com.backend.Biblioteca.application.dto.response.ExemplarResponseDTO;
+import com.backend.Biblioteca.application.service.ExemplarService;
+import com.backend.Biblioteca.domain.enums.StatusExemplar;
+import com.backend.Biblioteca.infrastructure.config.SecurityConfig;
+import com.backend.Biblioteca.infrastructure.security.JwtUtil;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+import tools.jackson.databind.ObjectMapper;
+
+@WebMvcTest(ExemplarControllerTest.class)
+@Import(SecurityConfig.class)
+public class ExemplarControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockitoBean
+    private ExemplarService service;
+
+    @MockitoBean
+    private JwtUtil jwtUtil;
+
+    private ExemplarResponseDTO exemplarResponse() {
+        return new ExemplarResponseDTO(1L, 1L, StatusExemplar.DISPONIVEL);
+    }
+
+    private ExemplarRequestDTO exemplarRequestValido() {
+        return new ExemplarRequestDTO(1L);
+    }
+}
