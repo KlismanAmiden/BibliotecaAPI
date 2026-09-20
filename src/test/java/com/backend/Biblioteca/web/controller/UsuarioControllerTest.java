e6932f029ab67922f3d77418f76c136fdfcc1bde
KlismanAmiden <Wkklisman08@Gmail.com>
2026-09-19 23:59:48 -0300

    fix: correct MockMvc request builder import

--- src/test/java/com/backend/Biblioteca/web/controller/UsuarioControllerTest.java
+++ src/test/java/com/backend/Biblioteca/web/controller/UsuarioControllerTest.java
@@ -23,7 +23,6 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
-import static org.springframework.http.RequestEntity.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
